82f20c787c9a2212509a432be295a1bb527d3a31
Centos_chen <chen@localhost.localdomain>
2020-11-04 20:26:46 -0800

    myminishell

---
+++ myminishell/minishell.c
@@ -0,0 +1,91 @@
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+char g_Command[1024];
+
+int GetCommand()
+{
+     memset(g_Command, '\0', sizeof(g_Command)); 
+     printf("[minishell@localhost ~]");
+     fflush(stdout);
+     if(fgets(g_Command, sizeof(g_Command) - 1, stdin) == NULL)
+     {
+          printf("fgets error\n");   
+          return -1;                
+     }
+     return 0;
+}
+
+
+int ExecCommand(char* argv[])
+{
+     if(argv[0] == NULL)
+     {    
+        printf("ExecCommand error\n");
+        return -1;                         
+     }  
+     pid_t pid = fork();      
+     if(pid < 0)
+     {
+         perror("fork");
+         return -1;                               
+     }
+     else if(pid == 0)
+     {
+         execvp(argv[0], argv); 
+         exit(0);
+     }
+     else
+     {
+         waitpid(pid,NULL,0);
+     }
+     return 0;
+}
+
+
+int DealCommand(char* command)
+{
+    if(!command && *command == '\0')
+     {
+        printf("command error\n");
+        return -1;                        
+     }
+    int argc = 0;
+    char* argv[1024] = {0};
+     while(*command)
+     {
+         while(!isspace(*command) && *command != '\0')
+         {
+             argv[argc] = command;
+             argc++;
+      while(!isspace(*command) && *command != '\0')
+      {
+         command++;
+                                      
+      }
+        *command = '\0';
+     }
+        command++;
+     }
+    argv[argc] = NULL;
+    ExecCommand(argv);
+    return 0;
+}
+int main()
+{
+    //读用户输入
+    while (1)
+    {
+        if (GetCommand()==-1)
+        {
+            continue;
+        }
+        DealCommand(g_Command);
+    }
+    return 0;
+}
